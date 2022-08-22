d0520c6179263c8291c9d9fbba48d1c22fd83df3
Eric Thornton <ericthornton43@gmail.com>
2022-08-22 14:56:34 -0600

    Complete first draft.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# clock
-Simple digital clock app
+# Clock
+
+A simple digital clock app. Watch the seconds of your life irreversibly tick away as you admire the simple practicality of this amazing app!
+
+**Live Preview**: https://et-codes.github.io/clock/
--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-const time = document.getElementById('time');
-const date = document.getElementById('date');
+const getTime = () => {
+  const time = new Date();
 
-time.textContent = '00:00:00';
-date.textContent = 'Day, Month Date Year';
+  const timeString = [
+    String(time.getHours()).padStart(2, '0'),
+    String(time.getMinutes()).padStart(2, '0'),
+    String(time.getSeconds()).padStart(2, '0')
+  ].join(':');
+
+  return timeString;
+};
+
+const getDate = () => {
+  const date = new Date();
+  const day = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+  ];
+  const month = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  const dateString = [
+    day[date.getDay()] + ',',
+    month[date.getMonth()],
+    date.getDate(),
+    date.getFullYear()
+  ].join(' ');
+
+  // The following technically meets the rules.  Allowed?
+  // const dateString = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(date);
+
+  return dateString;
+};
+
+const updateClock = () => {
+  const timeDiv = document.getElementById('time');
+  const dateDiv = document.getElementById('date');
+
+  timeDiv.innerHTML = getTime();
+  dateDiv.innerHTML = getDate();
+};
+
+updateClock();
+setInterval(updateClock, 1000);
